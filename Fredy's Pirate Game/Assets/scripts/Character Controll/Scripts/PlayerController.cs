383b449050f90eb94b42342644da566a148dc654
Francesco <francesco12382@gmail.com>
2018-08-30 10:25:06 +0800

    swimming
    Working on swimming mechanics

--- Fredy's Pirate Game/Assets/scripts/Character Controll/Scripts/PlayerController.cs
+++ Fredy's Pirate Game/Assets/scripts/Character Controll/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class PlayerController : MonoBehaviour {
 
@@ -18,6 +19,9 @@ public class PlayerController : MonoBehaviour {
 	float currentSpeed;
 	float velocityY;
 
+    [SerializeField]
+    WalkingStates state;
+
 	Animator animator;
 	Transform cameraT;
 	CharacterController controller;
@@ -26,26 +30,65 @@ public class PlayerController : MonoBehaviour {
 		animator = transform.GetChild(0).GetComponent<Animator> ();
 		cameraT = Camera.main.transform;
 		controller = GetComponent<CharacterController> ();
+        state = WalkingStates.Normal;
 	}
 
-	void Update () {
-		// input
-		Vector2 input = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
-		Vector2 inputDir = input.normalized;
-		bool running = Input.GetKey (KeyCode.LeftShift);
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Water")
+        {
+            state = WalkingStates.Swimming;
+        }
+    }
 
-		Move (inputDir, running);
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Water")
+        {
+            state = WalkingStates.Normal;
+        }
+    }
 
-		if (Input.GetKeyDown (KeyCode.Space)) {
-			Jump ();
-		}
-		// animator
-		float animationSpeedPercent = ((running) ? currentSpeed / runSpeed : currentSpeed / walkSpeed * .5f);
-		animator.SetFloat ("speedPercent", animationSpeedPercent, speedSmoothTime, Time.deltaTime);
+    void Update () {
 
+        switch (state)
+        {
+            case WalkingStates.Normal:
+                NormalWalk();
+                break;
+            case WalkingStates.Swimming:
+                Swim();
+                break;
+            case WalkingStates.Flying:
+                Debug.Log("State not yet created");
+                break;
+            default:
+                break;
+        }
 	}
 
-	void Move(Vector2 inputDir, bool running) {
+
+    #region Walking
+
+    private void NormalWalk()
+    {
+        // input
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Vector2 inputDir = input.normalized;
+        bool running = Input.GetKey(KeyCode.LeftShift);
+
+        Move(inputDir, running);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+        // animator
+        float animationSpeedPercent = ((running) ? currentSpeed / runSpeed : currentSpeed / walkSpeed * .5f);
+        animator.SetFloat("speedPercent", animationSpeedPercent, speedSmoothTime, Time.deltaTime);
+    }
+
+    void Move(Vector2 inputDir, bool running) {
 		if (inputDir != Vector2.zero) {
 			float targetRotation = Mathf.Atan2 (inputDir.x, inputDir.y) * Mathf.Rad2Deg + cameraT.eulerAngles.y;
 			transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, GetModifiedSmoothTime(turnSmoothTime));
@@ -66,14 +109,76 @@ public class PlayerController : MonoBehaviour {
 
 	}
 
-	void Jump() {
-		if (controller.isGrounded) {
-			float jumpVelocity = Mathf.Sqrt (-2 * gravity * jumpHeight);
-			velocityY = jumpVelocity;
-		}
-	}
+    void Jump()
+    {
+        if (controller.isGrounded)
+        {
+            float jumpVelocity = Mathf.Sqrt(-2 * gravity * jumpHeight);
+            velocityY = jumpVelocity;
+        }
+    }
+    #endregion
 
-	float GetModifiedSmoothTime(float smoothTime) {
+    #region swimming
+
+    private void Swim()
+    {
+
+        // input
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Vector2 inputDir = input.normalized;
+        bool running = Input.GetKey(KeyCode.LeftShift);
+
+        SwimMovement(inputDir, running);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SwimUp();
+        }
+        // animator
+        float animationSpeedPercent = ((running) ? currentSpeed / runSpeed : currentSpeed / walkSpeed * .5f);
+        animator.SetFloat("speedPercent", animationSpeedPercent, speedSmoothTime, Time.deltaTime);
+    }
+
+    void SwimMovement(Vector2 inputDir, bool racing)
+    {
+        if (inputDir != Vector2.zero)
+        {
+            float targetRotation = Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg + cameraT.eulerAngles.y;
+            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, GetModifiedSmoothTime(turnSmoothTime));
+        }
+
+        float targetSpeed = ((racing) ? runSpeed : walkSpeed) * inputDir.magnitude;
+        currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, GetModifiedSmoothTime(speedSmoothTime));
+
+        //If we have vertical velocity then dampen it until it is equal to 0
+        if (velocityY > 0)
+        {
+            velocityY += Time.deltaTime * gravity;
+        }
+        else
+        {
+            //Should create bobbing affect
+            velocityY += Time.deltaTime * -gravity * 0.1f;
+        }
+      
+        Vector3 velocity = transform.forward * currentSpeed + Vector3.up * velocityY;
+
+        controller.Move(velocity * Time.deltaTime);
+        currentSpeed = new Vector2(controller.velocity.x, controller.velocity.z).magnitude;
+
+  
+
+    }
+
+    void SwimUp()
+    {
+        float jumpVelocity = Mathf.Sqrt(-2 * gravity * jumpHeight);
+        velocityY = jumpVelocity;
+    }
+    #endregion
+
+    float GetModifiedSmoothTime(float smoothTime) {
 		if (controller.isGrounded) {
 			return smoothTime;
 		}
